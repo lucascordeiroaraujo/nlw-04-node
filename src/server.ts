8da0fc91e017ab2f199fbb9134df0ced090fc261
Lucas Cordeiro Araujo <lucascordeiroaraujo@gmail.com>
2021-02-23 17:44:49 -0300

    :card_file_box: connection to the database

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
+import 'reflect-metadata'
+
 import express from "express";
+
+import './database'
+
+import { router } from './routes'
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "NLW 04" });
-});
+app.use(express.json())
 
-app.post("/", (request, response) => {
-  //
-
-  return response.json({ message: "Dados salvos com sucesso" });
-});
+app.use(router)
 
 app.listen(3333, () => {
   console.log("Server is running 🚀");
